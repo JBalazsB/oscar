9d948ff35bbddf6242b1956cc4754a283b1a0419
JBalazsB <balazsjozan910@gmail.com>
2025-03-10 13:18:22 +0100

    Készen van az Oscarrestkezelo.php

--- dbvezerlopdo.php
+++ dbvezerlopdo.php
@@ -1,57 +1,44 @@
 <?php
-
-class DBController
+class DBController 
 {
-private $conn = null;
+    private $conn = null;
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $database = "oscar";
 
-private $host = "localhost";
-
-private $user = "root";
-
-private $password = "";
-
-private $database = "oscar";
-
-
-function __construct()
-{
-    $this->connectDB();
-}
-
-
-function connectDB()
-{
-    try
+    function __construct() 
     {
-        $this->conn = NEW PDO("mysql:host={$this->host};
-        dbname = {$this->database};charset=utf8",
-        $this->user,$this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE PDO::ERRMODE_EXCEPTION);
-    }
-    catch
-    {
-        die("Connection failed: " . $e->getMessage())
-    }
-}
-
-function executeSelectQuery($query, $params =[])
-{
-    try
-    {
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute($params);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    catch(PDOException $e)
-    {
-        die("Query failed: " . $e->getMessage());
+        $this->connectDB();
     }
 
-    function closeDB()
+    function connectDB() 
     {
-        $this->conn=null;
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};
+            dbname={$this->database};charset=utf8", 
+            $this->user, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, 
+            PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    function executeSelectQuery($query, $params = []) {
+        try 
+        {
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute($params);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die("Query failed: " . $e->getMessage());
+        }
+    }
+
+    function closeDB() {
+        $this->conn = null;
     }
 }
-}
-
 ?>
+
